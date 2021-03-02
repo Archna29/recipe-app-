51b4b49b2a47aa05cee56e17acf02a27c8d700ff
Archna Bhardwaj <55014509+Archna29@users.noreply.github.com>
2021-03-02 11:34:22 +0700

    Added dietlabels in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,6 +11,7 @@ return(
   <div className="card-body">
     <h5 className="card-title">{props.label}</h5>
     <p className="card-text">{props.calories} Calories</p>
+   <p className="card-text">{props.dietLabels}</p>
     <a href="#" className="btn btn-info w-30 m-3 " onClick={showingrediant}>Ingredients</a>
     <a href={props.mainrecipe} className="btn btn-success w-40 m-3 ">Recipe</a>
   {show?<h6>{props.ingrediant}</h6> :null}
